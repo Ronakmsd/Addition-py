15defb4b87f0402a9ab5c0eec20ae098e0516af2
Ronakmsd <ronak.17498@sakec.ac.in>
2024-10-23 21:58:06 -0700

    Add files via upload

--- Ronak.py
+++ Ronak.py
@@ -1,4 +1,7 @@
-x = input("Type a number: ")
-y = input("Type another number: ")
-sum = int(x) + int(y)
-print("The sum is: ", sum)
+# Python code to demonstrate the working of gcd()
+# importing "math" for mathematical operations
+import math
+
+# prints 12
+print("The gcd of 60 and 48 is : ", end="")
+print(math.gcd(60, 48))
